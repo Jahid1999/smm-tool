9609a939cdc1c9f77da3e6b2c53dfa2d7f757723
mahdeehasanbd1010 <bsse1010@iit.du.ac.bd>
2022-08-24 02:16:18 +0600

    change variable name

--- halsted.py
+++ halsted.py
@@ -9,33 +9,33 @@ keywords = ["function", "global", "for", "end", "while", "if", "else", "elseif",
             "try", "catch", "end", "const", "import", "export", "type", "return",
             "true", "false", "in", "abstract", "module", "continue", "do", "join"]
 
-singleline_comment_op = "#"
-multiline_comment_start_op = "#="
-multiline_comment_end_op = "=#"
+comment_sign = "#"
+# multiline_comment_start_op = "#="
+# multiline_comment_end_op = "=#"
 
 n1 = {}
 n2 = {}
 
 
-def filter_token(token):
-    tok = token
-    while tok:
-        tok = break_token(tok)
+def token_filtration(token):
+    temp_token = token
+    while temp_token:
+        temp_token = break_token(temp_token)
 
 
 def break_token(token):
-    op_pos = len(token)
-    for op in operators:
-        if token.startswith(op):
-            if op not in n1:
-                n1[op] = 1
+    operator_position = len(token)
+    for operator in operators:
+        if token.startswith(operator):
+            if operator not in n1:
+                n1[operator] = 1
             else:
-                n1[op] += 1
-            return token[len(op):]
-        if op in token:
-            op_pos = min(op_pos, token.find(op))
+                n1[operator] += 1
+            return token[len(operator):]
+        if operator in token:
+            operator_position = min(operator_position, token.find(operator))
 
-    remaining_token = token[:op_pos]
+    remaining_token = token[:operator_position]
     for keyword in keywords:
         if remaining_token == keyword:
             print(remaining_token)
@@ -53,10 +53,10 @@ def break_token(token):
             n2[remaining_token] += 1
             # print("***",remaining_token)
 
-    return token[op_pos:]
+    return token[operator_position:]
 
 
-def measure_halstead(N1, N2, n1, n2):
+def calculate_halstead_metrics(N1, N2, n1, n2):
     Vocabulary = n1 + n2
     Volume = (N1 + N2) * math.log(Vocabulary, 2)
     Difficulty = ((n1 / 2) * (N2 / n2))
@@ -68,32 +68,32 @@ def measure_halstead(N1, N2, n1, n2):
     print("Effort: ", Effort)
 
 
-def filter_comments(sourcecode_file):
-    singleline_comment_op_pos = -1
-    multiline_comment_start_op_pos = -1
-    multiline_comment_end_op_pos = -1
+def comments_section_filtration(coded_file):
+    comment_sign_position = -1
+    # multiline_comment_start_operator_position = -1
+    # multiline_comment_end_operator_position = -1
     filtered_lines = []
     inside_comment = False
 
-    file = open(sourcecode_file)
+    file = open(coded_file)
     for line in file:
         if not line.strip():
             continue
-        if singleline_comment_op in line:
-            singleline_comment_op_pos = line.find(singleline_comment_op)
+        if comment_sign in line:
+            comment_sign_position = line.find(comment_sign)
 
-        if singleline_comment_op_pos != -1:
-            filtered_lines.append(line[:singleline_comment_op_pos])
+        if comment_sign_position != -1:
+            filtered_lines.append(line[:comment_sign_position])
         else:
             filtered_lines.append(line)
 
-        singleline_comment_op_pos = -1
+        comment_sign_position = -1
 
     return filtered_lines
 
 
-def main(sourcecode_file):
-    lines = filter_comments(sourcecode_file)
+def main(coded_file):
+    lines = comments_section_filtration(coded_file)
 
     # print("Lines of Code: ", len(lines))
     for line in lines:
@@ -107,7 +107,7 @@ def main(sourcecode_file):
         tokens = line.strip().split()
         tokens = tokens + string_line
         for token in tokens:
-            filter_token(token)
+            token_filtration(token)
 
     for key, value in n1.items():
         print(key + " = " + str(value))
@@ -115,13 +115,13 @@ def main(sourcecode_file):
     for key, value in n2.items():
         print(key + " = " + str(value))
 
-    print("Operator(n1): ", n1)
-    print("Operand(n2): ", n2)
-    measure_halstead(sum(n1.values()), sum(n2.values()), len(n1), len(n2))
+    print("JSON of n1: ", n1)
+    print("JSON of n2: ", n2)
+    calculate_halstead_metrics(sum(n1.values()), sum(n2.values()), len(n1), len(n2))
 
 
 if __name__ == "__main__":
-    argn = len(sys.argv)
+    # argn = len(sys.argv)
     main("test.py")
 
 # https://github.com/IntelLabs/HPAT.jl/blob/master/examples/queries_devel/q26/halstead-calculator.py
